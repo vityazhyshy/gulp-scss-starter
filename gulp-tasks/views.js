5bd2ae4978a182b92af55a113956260b13022af5
vityazhyshy <109218243+vityazhyshy@users.noreply.github.com>
2023-08-15 11:00:36 +0000

    view.js

--- gulp-tasks/views.js
+++ gulp-tasks/views.js
@@ -7,18 +7,20 @@ import gulpif from "gulp-if";
 import replace from "gulp-replace";
 import browsersync from "browser-sync";
 import yargs from "yargs";
+import formatHTML from 'gulp-format-html';
 
 const argv = yargs.argv,
     production = !!argv.production;
 
-gulp.task("views", () => {
+gulp.task('views', () => {
     return gulp.src(paths.views.src)
         .pipe(include({
-            prefix: "@@",
-            basepath: "@file"
+            prefix: '@@',
+            basepath: '@file'
         }))
-        .pipe(gulpif(production, replace(".css", ".min.css")))
-        .pipe(gulpif(production, replace(".js", ".min.js")))
+        .pipe(formatHTML())
+        .pipe(gulpif(production, replace('.css', '.min.css')))
+        .pipe(gulpif(production, replace('.js', '.min.js')))
         .pipe(gulp.dest(paths.views.dist))
         .pipe(browsersync.stream());
 });
